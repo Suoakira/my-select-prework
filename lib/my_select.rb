99e389ca32c8f5e9ad5ecb3d8836ca3481477fd8
Stephen Courtier <steves0089@gmail.com>
2018-09-11 09:44:51 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
   new_collection = [] 
     while i < collection.size
     block_given?
-    
+    new_collection.push(yield(collection[i]))
     i += 1
   end
   new_collection
